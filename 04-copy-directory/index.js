22dcc0febf379e1e389667ce1fcb4b51216dd7f9
Kinskaya <64421675+Kinskaya@users.noreply.github.com>
2021-11-10 20:54:48 +0300

    fix: fix implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 
-
 function copyDir() {
   const filePath = path.dirname(__filename);
   let current, copy;
@@ -11,6 +10,17 @@ function copyDir() {
       if (files[i].isDirectory()) {
         current = files[i].name;
         copy = `${files[i].name}-copy`;
+
+        fs.readdir(path.join(filePath, copy), (err, files) => {
+          if (err) {
+            console.log(err);
+          }
+          for (let file of files) {
+            fs.unlink(path.join(path.join(filePath, copy), file), err => {
+              if (err) throw err;
+            });
+          }
+        });
 
         if (files[i].isDirectory() !== copy) {
           fs.mkdir(path.join(filePath, copy), { recursive: true }, err => {
@@ -22,7 +32,7 @@ function copyDir() {
               if (err) {
                 console.log(err);
               }
-              for (let i = 0; i < files.length; i += 1) {
+              for (let i = 0; i < files.length; i++) {
                 fs.copyFile(`${path.join(filePath, current)}/${files[i]}`, `${path.join(filePath, copy)}/${files[i]}`, err => {
                   if (err) {
                     console.log(err);
